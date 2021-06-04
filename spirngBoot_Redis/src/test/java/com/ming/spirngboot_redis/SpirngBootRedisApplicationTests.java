8173f445688a8e28488942f5b64735b63197a624
NamingCao <NamingCao@aliyun.com>
2021-06-04 19:46:04 +0800

    First commit

---
+++ spirngBoot_Redis/src/test/java/com/ming/spirngboot_redis/SpirngBootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.spirngboot_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngBootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/com/ming/springbootlearn/SpringBootLearnApplication.java
@@ -0,0 +1,16 @@
+package com.ming.springbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Cao
+ */
+@SpringBootApplication
+public class SpringBootLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearnApplication.class, args);
+    }
+
+}
